48f40b05f20cf8266cdde85f88f0fe27c8e18524
Jean Marques <jmichelmarques21@outlook.com>
2023-04-19 11:24:59 -0300

    ...

--- ExerciciosEstruturaCondicional/Program.cs
+++ ExerciciosEstruturaCondicional/Program.cs
@@ -105,7 +105,29 @@ namespace ExerciciosEstruturaCondicional {
 
             */
 
+            // Exercício 06 - Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos seguintes intervalos ([0,25], [25,50], [50,75], [75,100]) este valor se encontra. Obviamente se o valor não estiver em nenhum desses intervalos, deverá ser impressa a mensagem "Fora de intervalo".
 
+
+            Console.WriteLine("Digite um valor: ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            if (valor < 0 || valor > 100) {
+                Console.WriteLine("Fora de intervalo.");
+            }
+            else if (valor <= 25) {
+                Console.WriteLine("Intervalo [0,25].");
+            }
+            else if (valor <= 50) {
+                Console.WriteLine("Intervalo [25,50].");
+            }
+            else if (valor <= 75) {
+                Console.WriteLine("Intervalo [50,75].");
+            }
+            else {
+                Console.WriteLine("Intervalo [75,100].");
+            }
+          
 
 
         }
