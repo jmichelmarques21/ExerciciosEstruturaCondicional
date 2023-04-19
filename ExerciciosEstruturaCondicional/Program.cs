31050f92ddf28b5525237516356beb918c9b8160
Jean Marques <jmichelmarques21@outlook.com>
2023-04-19 11:44:15 -0300

    ...

--- ExerciciosEstruturaCondicional/Program.cs
+++ ExerciciosEstruturaCondicional/Program.cs
@@ -105,7 +105,7 @@ namespace ExerciciosEstruturaCondicional {
 
             */
 
-            // Exercício 06 - Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos seguintes intervalos ([0,25], [25,50], [50,75], [75,100]) este valor se encontra. Obviamente se o valor não estiver em nenhum desses intervalos, deverá ser impressa a mensagem "Fora de intervalo".
+            /* Exercício 06 - Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos seguintes intervalos ([0,25], [25,50], [50,75], [75,100]) este valor se encontra. Obviamente se o valor não estiver em nenhum desses intervalos, deverá ser impressa a mensagem "Fora de intervalo".
 
 
             Console.WriteLine("Digite um valor: ");
@@ -127,6 +127,39 @@ namespace ExerciciosEstruturaCondicional {
             else {
                 Console.WriteLine("Intervalo [75,100].");
             }
+
+            */
+
+            // Exercício 07 - Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano. A seguir, determine o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0). Se o ponto estiver na origim, escreva a mensagem "Origem". Se o ponto estiver sobre um dos eixos escreva "Eixo X" ou "Eixo Y", conforme for a situação. (Quadrantes de Q1 a Q4 seguindo ordem anti-horária).
+            Console.WriteLine("Digite dois valores (na mesma linha: )");
+            string[] valores = Console.ReadLine().Split(' ');
+            double x = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            double y = double.Parse(valores[1], CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+
+            if (x > 0 && y > 0) {
+                Console.WriteLine("Q1.");
+            }
+            else if (x < 0 && y > 0) {
+                Console.WriteLine("Q2.");
+            }
+            else if (x < 0 && y < 0) {
+                Console.WriteLine("Q3.");
+            }
+            else if (x > 0 && y < 0) {
+                Console.WriteLine("Q4.");
+            }
+            else if (x == 0 && y == 0) {
+                Console.WriteLine("Origem.");
+            }
+            else if (x == 0 && y > 0 || y < 0) {
+                Console.WriteLine("Eixo X.");
+            }
+            else {
+                Console.WriteLine("Eixo Y.");
+            }
+
           
 
 
