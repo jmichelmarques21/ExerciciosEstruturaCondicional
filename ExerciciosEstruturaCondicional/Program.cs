ba0bca35a56df73d09a4dd9f17555d2cd7417e18
Jean Marques <jmichelmarques21@outlook.com>
2023-04-19 09:42:24 -0300

    .

--- ExerciciosEstruturaCondicional/Program.cs
+++ ExerciciosEstruturaCondicional/Program.cs
@@ -5,7 +5,7 @@ namespace ExerciciosEstruturaCondicional {
     class Program {
         static void Main(string[] args) {
 
-            //Exercicio 01 - Fazer um programa para ler um número inteiro e depois dizer se este número é negativo ou não.
+            /* Exercicio 01 - Fazer um programa para ler um número inteiro e depois dizer se este número é negativo ou não.
 
             Console.WriteLine("Digite um número inteiro: ");
             int num = int.Parse(Console.ReadLine());
@@ -15,6 +15,25 @@ namespace ExerciciosEstruturaCondicional {
             } else {
                 Console.WriteLine("Negativo!");
             }
+
+            */
+
+            /* Exercício 02 - Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+
+            Console.WriteLine("Digite um número inteiro: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0) {
+                Console.WriteLine("Número par!");
+            }
+            else {
+                Console.WriteLine("Número ímpar!");
+            }
+
+            */
+
+            // Exercício 03 - 
+
 
 
 
