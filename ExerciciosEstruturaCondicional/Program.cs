f920518f0fdc62b69af05faaa8b03dd67789c1db
Jean Marques <jmichelmarques21@outlook.com>
2023-04-19 12:14:13 -0300

    ...

--- ExerciciosEstruturaCondicional/Program.cs
+++ ExerciciosEstruturaCondicional/Program.cs
@@ -32,7 +32,9 @@ namespace ExerciciosEstruturaCondicional {
 
             */
 
-            /* Exercício 03 - Leia 2 valores inteiros (A e B). Após isso, o programa deve mostrar uma mensagem "São múltiplos" ou "Não são múltiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem ser digitados em ordem crescente ou decrescente.
+            /* Exercício 03 - Leia 2 valores inteiros (A e B). Após isso, o programa deve mostrar uma mensagem "São múltiplos" 
+             * ou "Não são múltiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem ser digitados 
+             * em ordem crescente ou decrescente.
 
             Console.WriteLine("Digite dois números inteiros (na mesma linha): ");
             string[] numeros = Console.ReadLine().Split(' ');
@@ -48,7 +50,8 @@ namespace ExerciciosEstruturaCondicional {
 
             */
 
-            /* Exercício 04 - Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.
+            /* Exercício 04 - Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o 
+             * mesmo pode começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.
 
             Console.WriteLine("Digite o horário de início e o de fim do jogo (na mesma linha): ");
             string[] horarios = Console.ReadLine().Split(' ');
@@ -69,7 +72,8 @@ namespace ExerciciosEstruturaCondicional {
             
             */
 
-            /* Exercício 05 - Com base nos dados abaixo, escreva um programa que leia o código de um item e a quantidade do mesmo. A seguir, calcule e mostre o valor da conta a pagar.
+            /* Exercício 05 - Com base nos dados abaixo, escreva um programa que leia o código de um item e a quantidade do mesmo. 
+             * A seguir, calcule e mostre o valor da conta a pagar.
                 codigo (1) - item (cachorro quente) - preço (R$4,00)
                 codigo (2) - item (       x-salada) - preço (R$4,50)
                 codigo (3) - item (        x-bacon) - preço (R$5,00)
@@ -105,7 +109,9 @@ namespace ExerciciosEstruturaCondicional {
 
             */
 
-            /* Exercício 06 - Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos seguintes intervalos ([0,25], [25,50], [50,75], [75,100]) este valor se encontra. Obviamente se o valor não estiver em nenhum desses intervalos, deverá ser impressa a mensagem "Fora de intervalo".
+            /* Exercício 06 - Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos 
+             * seguintes intervalos ([0,25], [25,50], [50,75], [75,100]) este valor se encontra. 
+             * Obviamente se o valor não estiver em nenhum desses intervalos, deverá ser impressa a mensagem "Fora de intervalo".
 
 
             Console.WriteLine("Digite um valor: ");
@@ -130,7 +136,13 @@ namespace ExerciciosEstruturaCondicional {
 
             */
 
-            // Exercício 07 - Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano. A seguir, determine o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0). Se o ponto estiver na origim, escreva a mensagem "Origem". Se o ponto estiver sobre um dos eixos escreva "Eixo X" ou "Eixo Y", conforme for a situação. (Quadrantes de Q1 a Q4 seguindo ordem anti-horária).
+            /* Exercício 07 - Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um 
+             * plano. A seguir, determine o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na 
+             * origem (x = y = 0). Se o ponto estiver na origim, escreva a mensagem "Origem". 
+             * Se o ponto estiver sobre um dos eixos escreva "Eixo X" ou "Eixo Y", conforme for a situação. 
+             * (Quadrantes de Q1 a Q4 seguindo ordem anti-horária).
+             * 
+             * 
             Console.WriteLine("Digite dois valores (na mesma linha: )");
             string[] valores = Console.ReadLine().Split(' ');
             double x = double.Parse(valores[0], CultureInfo.InvariantCulture);
@@ -160,7 +172,31 @@ namespace ExerciciosEstruturaCondicional {
                 Console.WriteLine("Eixo Y.");
             }
 
-          
+            */
+
+            /* Exercício 08 - Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em pagar seus impostos,
+             * pois sabem que nele não existem políticos corruptos e os recursos arrecadados são utilizados em benefício da 
+             * população, sem qualquer desvio. A moeda deste país é o Rombus, cujo símbolo é o R$.
+             * 
+             * Leia um valor com duas casas decimais, equivalente ao salário de uma pessoa em Lisarb. Em seguida, calcule e
+             * mostre o valor que esta pessoa deve pagar de Imposto de Rendam, seguindo os dados abaixo:
+             * 
+             * Renda (de R$0000.00 até R$2000.00) - IR (Isento)
+             * Renda (de R$2000.01 até R$3000.00) - IR (    8%)
+             * Renda (de R$3000.01 até R$4500.00) - IR (   18%)
+             * Renda (          acima de 4500.00) - IR (   28%)
+             * 
+             * Lembre que, se o salário for R$3002.00, a taxa que incide é de 8% apenas sobre R$1000.00, pois a faixa de
+             * salário que fica de R$0.00 até R$2000.00 é isenta de Imposto de Renda.
+             * 
+             * */
+
+            Console.WriteLine("Digite o salário: ");
+            double salario = double.Parse(Console.ReadLine());
+
+
+
+
 
 
         }
