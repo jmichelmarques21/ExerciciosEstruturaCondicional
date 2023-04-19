1eb4924e857e9f78c136fade6273dce9e322115f
Jean Marques <jmichelmarques21@outlook.com>
2023-04-19 09:35:31 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosEstruturaCondicional/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciciosEstruturaCondicional {
+    class Program {
+        static void Main(string[] args) {
+
+            //Exercicio 01 - Fazer um programa para ler um número inteiro e depois dizer se este número é negativo ou não.
+
+            Console.WriteLine("Digite um número inteiro: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num >= 0) {
+                Console.WriteLine("Não negativo!");
+            } else {
+                Console.WriteLine("Negativo!");
+            }
+
+
+
+
+        }
+    }
+}
