0fad4bbdad88a7f47f7bd26844cd37f9a5b9fee0
Jean Marques <jmichelmarques21@outlook.com>
2023-04-19 10:53:57 -0300

    ...

--- ExerciciosEstruturaCondicional/Program.cs
+++ ExerciciosEstruturaCondicional/Program.cs
@@ -48,7 +48,7 @@ namespace ExerciciosEstruturaCondicional {
 
             */
 
-            // Exercício 04 - Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.
+            /* Exercício 04 - Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.
 
             Console.WriteLine("Digite o horário de início e o de fim do jogo (na mesma linha): ");
             string[] horarios = Console.ReadLine().Split(' ');
@@ -67,6 +67,43 @@ namespace ExerciciosEstruturaCondicional {
                 Console.WriteLine("O jogo durou 24 horas.");
             }
             
+            */
+
+            /* Exercício 05 - Com base nos dados abaixo, escreva um programa que leia o código de um item e a quantidade do mesmo. A seguir, calcule e mostre o valor da conta a pagar.
+                codigo (1) - item (cachorro quente) - preço (R$4,00)
+                codigo (2) - item (       x-salada) - preço (R$4,50)
+                codigo (3) - item (        x-bacon) - preço (R$5,00)
+                codigo (4) - item (torrada simples) - preço (R$2.00)
+                codigo (5) - item (   refrigerante) - preço (R$1,50)
+            
+
+            Console.WriteLine("Digite o código do produto e a quantidade comprada (na mesma linha): ");
+            string[] pedido = Console.ReadLine().Split(' ');
+            int codigo = int.Parse(pedido[0]);
+            int quantidade = int.Parse(pedido[1]);
+
+            double total;
+
+            if (codigo == 1) {
+                total = quantidade * 4.0;
+            }
+            else if (codigo == 2) {
+                total = quantidade * 4.5;
+            }
+            else if (codigo == 3) {
+                total = quantidade * 5.0;
+            }
+            else if (codigo == 4) {
+                total = quantidade * 2.0;
+            }
+            else {
+                total = quantidade * 1.5;
+            }
+
+
+            Console.WriteLine("TOTAL A PAGAR: R$" + total.ToString("F2"), CultureInfo.InvariantCulture);
+
+            */
 
 
 
