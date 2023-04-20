43de7c06b7c27f3390efda4c2d01a66fd002b426
Jean Marques <jmichelmarques21@outlook.com>
2023-04-20 09:39:39 -0300

    ...

--- ExerciciosEstruturaCondicional/Program.cs
+++ ExerciciosEstruturaCondicional/Program.cs
@@ -189,12 +189,33 @@ namespace ExerciciosEstruturaCondicional {
              * Lembre que, se o salário for R$3002.00, a taxa que incide é de 8% apenas sobre R$1000.00, pois a faixa de
              * salário que fica de R$0.00 até R$2000.00 é isenta de Imposto de Renda.
              * 
-             * */
+             *
 
             Console.WriteLine("Digite o salário: ");
-            double salario = double.Parse(Console.ReadLine());
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double imposto;
 
+            if (salario <= 2000.00) {
+                imposto = 0;
+            }
+           else if (salario <= 3000.00) {
+                imposto = (salario - 2000.00) * 0.08; 
+            }
+            else if (salario <= 4500.00) {
+                imposto = (salario - 3000.00) * 0.18 + 1000.00 * 0.08;
+            }
+            else {
+                imposto = (salario - 4500.00) * 0.28 + 1500.00 * 0.18 + 1000.00 * 0.08;
+            }
 
+            if (imposto == 0) {
+                Console.WriteLine("Isento!");
+            }
+            else {
+                Console.WriteLine("Imposto: R$" + imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            */
 
 
 
